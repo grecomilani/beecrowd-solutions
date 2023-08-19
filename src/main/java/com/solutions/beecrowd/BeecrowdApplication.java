41af0de94f64c0fae7142726a72bc0fb9ca30913
grecolima <greco_lima@hotmail.com>
2023-08-20 00:35:37 +0100

    First Commit

---
+++ src/main/java/com/solutions/beecrowd/BeecrowdApplication.java
@@ -0,0 +1,13 @@
+package com.solutions.beecrowd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeecrowdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeecrowdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solutions/beecrowd/BeecrowdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutions.beecrowd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeecrowdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
